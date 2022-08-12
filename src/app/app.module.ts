57a5a934c6eca12bb0b6dcf78ba0f66c9b8fddd3
Fenix Sampson <Fenix.sampson@gmail.com>
2022-08-12 17:32:51 -0400

    created review component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
